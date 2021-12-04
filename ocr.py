3d077f3d2712b498fb5d3b7376f9559805d1b6f5
EmmaChoi0 <yeseul_choi@brown.edu>
2021-12-04 13:58:21 -0500

    please work

--- ocr.py
+++ ocr.py
@@ -22,6 +22,8 @@ def train():
     length = y_train.shape[0]
     fixed_x_train = [[[]]]
     fixed_y_train = []
+    print(len(fixed_x_train))
+    print(len(fixed_y_train))
     print("separating first")
     for i in range(10000):
         if y_train[i] < 36:
@@ -29,6 +31,9 @@ def train():
             #fixed_y_train = np.append(fixed_y_train, y_train[i])
             fixed_x_train = fixed_x_train + [x_train[i][:][:]]
             fixed_y_train = fixed_y_train + [y_train[i]]
+            if (len(fixed_x_train) != len(fixed_y_train)):
+                print("Error:")
+                print(i)
         #print(i)
     
     #print("separating second")
@@ -84,6 +89,7 @@ def train():
     model.summary()
 
 def predict(images):
+
     pass
 
 
