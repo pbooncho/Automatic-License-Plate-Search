71d664c0d9a2c821dd01ac58268086b8d01b554e
Jay Sarva <sarva.jay.s@gmail.com>
2021-12-08 13:51:43 -0500

    use func preedict image in main

--- main.py
+++ main.py
@@ -3,27 +3,26 @@ from load_data import preprocessing
 from model import create_model
 from ocr import train, predict
 from segmentation import segmentImage
-
+from predict_box import predict_image
 import tensorflow as tf 
 import glob
 import cv2
 
-preprocessing()
+#preprocessing()
 
 file = open("boundingbox.csv")
 rows = np.loadtxt(file, delimiter=",")
 # use saved weights for ocr model
 license_plate_detect = tf.keras.models.load_model("my_model.h5")
 
-images = [cv2.imread(file) for file in glob.glob("preprocessed_data/resized_images/*.png")][:10]
+images = glob.glob("preprocessed_data/resized_images/*.png")[:10]
 
-license_plates_bounding_points = license_plate_detect.predict(images)
-
+license_plates_bounding_points = [predict_image(path, license_plate_detect) for path in images]
 
 
 for index, row in enumerate(license_plates_bounding_points):
-
-    segmented_plates = map(lambda subIm: segmentImage(images[index][subIm[2]:subIm[3],subIm[0]:subIm[1]), license_plates_bounding_points)
+    image = cv2.imread(license_plates_bounding_points[0])
+    segmented_plates = map(lambda subIm: segmentImage(image[subIm[1]:subIm[3],subIm[0]:subIm[2]), license_plates_bounding_points)
 
     for plate_nums in segmented_plates:
         all_nums = predict(plate_nums)
