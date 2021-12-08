a040741a8f5644aeeef3f895dee9b662927479eb
Jay Sarva <sarva.jay.s@gmail.com>
2021-12-08 13:46:37 -0500

    including images in main

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from ocr import train, predict
 from segmentation import segmentImage
 
 import tensorflow as tf 
+import glob
+import cv2
 
 preprocessing()
 
@@ -13,13 +15,15 @@ rows = np.loadtxt(file, delimiter=",")
 # use saved weights for ocr model
 license_plate_detect = tf.keras.models.load_model("my_model.h5")
 
+images = [cv2.imread(file) for file in glob.glob("preprocessed_data/resized_images/*.png")][:10]
+
 license_plates_bounding_points = license_plate_detect.predict(images)
 
 
 
-for row in license_plates_bounding_points:
+for index, row in enumerate(license_plates_bounding_points):
 
-    segmented_plates = map(lambda subIm: segmentImage(images[i][subIm[2]:subIm[3],subIm[0]:subIm[1]), license_plates_bounding_points)
+    segmented_plates = map(lambda subIm: segmentImage(images[index][subIm[2]:subIm[3],subIm[0]:subIm[1]), license_plates_bounding_points)
 
     for plate_nums in segmented_plates:
         all_nums = predict(plate_nums)
